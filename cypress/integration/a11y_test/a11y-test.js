97741860dc9b6918231294c0d30189b9ca37d376
Elliot Palmer <elliot.palmer@co-operativebank.co.uk>
2020-02-14 13:42:49 +0000

    Adding in comment why a11y test have been excluded and how to undo this

--- cypress/integration/a11y_test/a11y-test.js
+++ cypress/integration/a11y_test/a11y-test.js
@@ -1,3 +1,6 @@
+// The a11y test have been excluded as these will always fail
+// To run the test remove `ignoreTestFiles` from `cypress/plugins/index.js`
+
 const pages = ['entry_ad', 'windows', 'javascript_alerts', 'dropdown', 'tables', 'dynamic_controls'];
 
 describe('Accessibility Testing', () => {
