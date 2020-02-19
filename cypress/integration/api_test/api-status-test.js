07abbb5da2a16b4c2b35c965edfa923ec5cc72ef
Elliot Palmer <elliot.palmer@co-operativebank.co.uk>
2020-02-19 15:24:46 +0000

    refactoring of the api status test

--- cypress/integration/api_test/api-status-test.js
+++ cypress/integration/api_test/api-status-test.js
@@ -2,14 +2,11 @@ var array = ['entry_ad', 'windows', 'javascript_alerts', 'dropdown', 'tables', '
 
 //Just messing around with expecting status 200
 describe('API - Check HTTP Status', () => {
-    it('Retrun 200', () => {
-
-        var i = 0
-
-        for (i; i < array.length; i++) {
-            cy.request(array[i]).should((response) => {
+    array.forEach((url) => {
+        it('Retrun 200', () => {
+            cy.request(url).should((response) => {
                 expect(response.status).equal(200);
             });
-        }
+        });
     });
 });
